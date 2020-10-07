0af43f9be00a0eb3ca6d3b01f7823fee15f64567
Thea Van Rossum <rossum@embl.de>
2020-10-07 16:30:22 +0200

    add log scaling pre-pearson correlation; refactor code so not whole gene file is loaded into memory

--- src/subpopr/R/correlateSubpopProfileWithGeneProfiles.R
+++ src/subpopr/R/correlateSubpopProfileWithGeneProfiles.R
@@ -1,18 +1,20 @@
 
 
-# keggProfilesPath <- "/Users/rossum/Dropbox/PostDocBork/subspecies/toolDevelopment/subpopr/ES_PC/mapped/igc/incomplete/kegg/keggIncompleteRenamed.txt"
-# geneAbundancePath <- keggProfilesPath
-# species <- "357276"
-#outDir = "/Users/rossum/Dropbox/PostDocBork/subspecies/toolDevelopment/subpopr/ES_PC/metaSNV/fr11_v1/minFilter/"
+#geneAbundancePath <- "/g/scb2/bork/rossum/metagenomes/human/subspecGeoValidation/all_v3/geneContent/mapToPanGenomes/outputs/counts_unique_norm_sumByNogBySpecies.tsv"
+# geneFamilyType <- "geneFamily"
+# species <- "742765"
+# SAMPLE.ID.SUFFIX<-".subspec71.unique.sorted.bam"
+# outDir = "/g/scb2/bork/rossum/metagenomes/human/subspecGeoValidation/all_v3/subpopr/results_md_s257745/params.hr10.hs80.ps80.gs80/outputs/"
 #' @exportMethod
 #' @return dataframe with correlation scores
-correlateSubpopProfileWithGeneProfiles <- function(species,outDir,geneAbundancePath,geneFamilyType,corrMethod="pearson"){
+correlateSubpopProfileWithGeneProfiles <- function(species,outDir,geneAbundancePath,
+                                                   geneFamilyType,corrMethod="pearson"){
   # library(data.table) # for faster correlations
 
   allClustAbund <- read.table(paste(outDir,species,'_allClust_relativeAbund.tab',sep=''),sep='\t')
-
+  speciesAbund <- rowSums(allClustAbund)
+  
   allClustAbund <- mutate(allClustAbund, sampleName = rownames(allClustAbund))
-
 
   # remove clusters only see in less than 3 samples
   allClustAbundInMin3Samples <- allClustAbund %>%
@@ -26,27 +28,22 @@ correlateSubpopProfileWithGeneProfiles <- function(species,outDir,geneAbundanceP
   }
 
   allClustAbund <- allClustAbundInMin3Samples
-
-  # geneFamilyProfiles <- read_tsv(geneAbundancePath,comment = "#",col_names = T,
-  #                                cols(.default = col_number(),X1 = col_character())
-  #                                ) %>%
-  #   rename(geneFamily = X1) %>%
-  #   gather(key = "sampleName",value = "abundance",-geneFamily)
-  geneFamilyProfiles <- read_tsv(geneAbundancePath,comment = "#",col_names = T)
-  colnames(geneFamilyProfiles)[1] <- "geneFamily"
-  geneFamilyProfiles <- geneFamilyProfiles %>%
-    gather(key = "sampleName",value = "abundance",-geneFamily)
-
-
-  # only use samples that have gene abundance and subspecies frequency data
+  
+  # figure out which samples we need and only load data for those samples
   subspeciesSamples <- unique(allClustAbund$sampleName)
-  geneAbundSamples <- unique(geneFamilyProfiles$sampleName)
+  
+  # get sample names from the gene family profiles
+  geneFamilyColumnNames <- read_tsv(file = geneAbundancePath,n_max=1,
+                     comment = "#",trim_ws = T,col_names = T) %>% colnames()
+  geneAbundSamples <- geneFamilyColumnNames[-1]
+  
+  # only use samples that have gene abundance and subspecies frequency data
   samplesToUse <- intersect(unique(allClustAbund$sampleName), # subspeciesSamples
-                            unique(geneFamilyProfiles$sampleName)) # gene abund samples
-
+                            unique(geneAbundSamples)) # gene abund samples
+  geneAbundColNamesToUse <- samplesToUse
   # first try to fix with most common problem -- presence of extra stuff after file name e.g. [sampleID].subspec71.unique.sorted.bam
   if(length(samplesToUse) == 0 & exists("SAMPLE.ID.SUFFIX")){
-    namesWithoutSuffix <- geneFamilyProfiles$sampleName
+    namesWithoutSuffix <- geneAbundSamples
     namesWithSuffix <- paste0(namesWithoutSuffix,SAMPLE.ID.SUFFIX)
     samplesToUse <- intersect(namesWithSuffix, subspeciesSamples)
     if(length(samplesToUse)>0){
@@ -55,28 +52,52 @@ correlateSubpopProfileWithGeneProfiles <- function(species,outDir,geneAbundanceP
               "Now, out of ",
               length(subspeciesSamples)," samples with SNV data, ",
               length(samplesToUse)," also have gene abundance data.")
-      geneFamilyProfiles$sampleName <- namesWithSuffix
+      names(geneAbundSamples) <- namesWithSuffix
+      geneAbundColNamesToUse <- geneAbundSamples[samplesToUse]
+      newColumnNames <- namesWithSuffix
+      names(newColumnNames) <- namesWithoutSuffix
+      fixColNames <- T
+      #geneFamilyProfiles$sampleName <- namesWithSuffix
     }
   }
-
-  samplesToUse <- intersect(unique(allClustAbund$sampleName), # subspeciesSamples
-                            unique(geneFamilyProfiles$sampleName)) # gene abund samples
-
+  
   if(length(samplesToUse) == 0){
     stop(paste("No overlapping sample IDs between clustering and gene family abundance profiles.",
                " Are IDs formatted differently? Example clustering sample IDs: ",
                paste(head(n=3,unique(allClustAbund$sampleName)),collapse=", "),
                ". Example gene family sample IDs:",
-               paste(head(n=3,unique(geneFamilyProfiles$sampleName)),collapse=", "), sep=""))
+               paste(head(n=3,unique(geneAbundSamples)),collapse=", "), sep=""))
   }
+  
+                     
+  f <- function(x,pos){ 
+    # only keep the samples (columns) that have this species
+    x <- x[,colnames(x) %in% c(colnames(x)[1],geneAbundColNamesToUse) ] 
+    # only keep genes that are present
+    filter(x,rowSums(x[,-1])>0)
+  }
+  geneFamilyProfiles <- read_tsv_chunked(file = geneAbundancePath,
+                                                 comment = "#",trim_ws = T,col_names = T,
+                                                 chunk_size = 10000,
+                                                 callback = DataFrameCallback$new(f)
+  ) %>% rename(geneFamily = 1) # rename the first column  
+  
+  
+  # geneFamilyProfiles <- read_tsv(geneAbundancePath,comment = "#",col_names = T) %>%
+  #   rename(geneFamily = 1) %>%
+  #   gather(key = "sampleName",value = "abundance",-geneFamily)
 
+  if(fixColNames){
+    colnames(geneFamilyProfiles)[-1] <- newColumnNames[colnames(geneFamilyProfiles)[-1]]  
+  }
+  
   geneFamilyProfiles <- geneFamilyProfiles %>%
-    filter(sampleName %in% samplesToUse) %>%
+    gather(key = sampleName, value = abundance, -geneFamily) %>% 
     group_by(sampleName) %>%
-    mutate(koAbundNorm = abundance/sum(abundance)) %>% # should we do this?
+    mutate(koAbundNorm = abundance/sum(abundance)) %>%
     select(-abundance) %>%
     ungroup()
-
+  
   allClustAbund <- filter(allClustAbund, sampleName %in% samplesToUse) %>%
     rename(clustAbund = abundance) %>%
     mutate(cluster = sub(x = cluster, pattern = "^X",replacement = "")) %>% # remove X prefix that was introduced when cluster was column name
@@ -88,17 +109,19 @@ correlateSubpopProfileWithGeneProfiles <- function(species,outDir,geneAbundanceP
                  pull(geneFamily) %>% unique() %>% length()," geneFamily groups in ",
                length(unique(allClustAbund$sampleName))," samples"))
 
-  fullData <- full_join(geneFamilyProfiles, allClustAbund,by="sampleName") %>% as.data.table()
-  # for speed optimisation later
-  # print(system.time(c <- left_join(geneFamilyProfiles, allClustAbund,by="sampleName")))
-  # print(system.time(a <- as.data.table(geneFamilyProfiles)[as.data.table(allClustAbund),on="sampleName",allow.cartesian=TRUE])) # this is the fastest, but not by much
-  # print(system.time(b <- as.data.table(allClustAbund)[as.data.table(geneFamilyProfiles),on="sampleName",allow.cartesian=TRUE]))
 
   cor.test.mod <- function(x,y,method,exact,geneFamily,cluster){
     # if no variance in either value, don't compute correlation
     if(diff(range(x)) == 0){return(NULL)}
     if(diff(range(y)) == 0){return(NULL)}
 
+    # log 10 transform for pearson to reduce weight of rare, large values
+    if(method == "pearson"){
+      pseudocount <- min(c(x,y))/100
+      x <- log10(x+pseudocount)
+      y <- log10(y+pseudocount)
+    }
+    
     res <- cor.test(x,y,method=method,exact=exact)
     if(!is.numeric(res$statistic)) {return(NULL)}
     res$parameter <- F # NULL causes error for data.table
@@ -117,8 +140,14 @@ correlateSubpopProfileWithGeneProfiles <- function(species,outDir,geneAbundanceP
     res$nObs <- length(x)
     return(res)
   }
-
-  corr <- fullData[, cor.test.mod(koAbundNorm,clustAbund,method=corrMethod,exact = !(corrMethod=="spearman"),geneFamily,cluster ),
+  
+  # use data table for faster corr computation
+  # this is fast enough but could re-write to avoid this join and use less memory
+  fullData <- inner_join(geneFamilyProfiles, allClustAbund,by="sampleName") %>% as.data.table()
+  corr <- fullData[, cor.test.mod(koAbundNorm,clustAbund,
+                                  method=corrMethod,
+                                  exact = !(corrMethod=="spearman"),
+                                  geneFamily,cluster ),
                     by = c("geneFamily","cluster")]
 
   corr <- select(corr, -data.name, -parameter)
@@ -128,3 +157,15 @@ correlateSubpopProfileWithGeneProfiles <- function(species,outDir,geneAbundanceP
 
   return(corr)
 }
+
+
+
+# correlate with species profile
+
+
+# filter correlations
+# min Pearson correlation 0.8 - computed on log10‐transformed relative abundances
+# min Spearman correlation 0.6
+
+
+
